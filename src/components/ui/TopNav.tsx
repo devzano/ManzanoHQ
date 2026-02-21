ff3c42d163068d38a1c86bdc7d5d867256923af7
Ruben <rubenizag@gmail.com>
2026-02-20 21:39:18 -0500

    profoilo

--- src/components/ui/TopNav.tsx
+++ src/components/ui/TopNav.tsx
@@ -99,7 +99,7 @@ export default function TopNav({ hideActions }: TopNavProps) {
               </Link>
 
               <a
-                href="mailto:manzanohq@gmail.com"
+                href="https://rubenmanzano.com"
                 className="rounded-xl border px-3 py-1.5 bg-white/5 hover:bg-white/10 transition"
                 style={{ borderColor: `rgba(${ACCENT_RGB},0.35)` }}
               >
